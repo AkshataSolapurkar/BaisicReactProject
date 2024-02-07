76b08b0567d0cf15ba85ccb54c003eb20c7cfc14
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-02-07 09:44:43 +0530

    Tours Updated

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,27 @@
-import React, { useState } from 'react';
+import React, {  useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,price,info,name,image,removeTour}) => {
+  const [showmore,setshowmore] = useState(false)
+
+  return (
+    <section className='single-tour'>
+      <img src={image} alt={name} />
+      <div className="tour-info">
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+      </div>
+      <div>
+        <div className='Parabtn'>
+        <p>{showmore?info :`${info.substring(0,200)}...`}
+        <button onClick={()=>setshowmore(!showmore)}>{showmore? `Show Less` : `Show More`}</button>
+        </p>
+        
+        </div>
+      
+      <button onClick={()=>removeTour(id)} className='delete-btn'>Not Intrested</button>
+      </div>
+    </section>
+  )
 };
 
 export default Tour;
